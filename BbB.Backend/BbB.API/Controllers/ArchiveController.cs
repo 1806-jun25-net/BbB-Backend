d23484ab3f50aef6c6a3208b2bc6044d47cd7cb3
Zach <zjturley@gmail.com>
2018-07-27 14:37:48 -0400

    Added Archive Controller (empty)

---
+++ BbB.Backend/BbB.API/Controllers/ArchiveController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BbB.Data;
+using BbB.Library;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BbB.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArchiveController : Controller
+    {
+        private readonly DataRepository data;
+        
+        public ArchiveController(DataRepository repository)
+        {
+            data = repository;
+        }
+
+    }
+}
