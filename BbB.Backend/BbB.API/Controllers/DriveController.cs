1951f8c605f1b51ca4d725987001de8061d18707
Zach <zjturley@gmail.com>
2018-08-01 10:54:48 -0400

    Drive Controller 1.0, associated methods

--- BbB.Backend/BbB.API/Controllers/DriveController.cs
+++ BbB.Backend/BbB.API/Controllers/DriveController.cs
@@ -20,16 +20,71 @@ namespace BbB.API.Controllers
             data = repository;
         }
 
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<Library.Drive>> Get(int id)
-        //{
-        //    data.getdr
-        //}
-
-        [HttpGet("{company}")]
-        public async Task<ActionResult<List<Library.Drive>>> Get(string company)
+        /// <summary>
+        /// Get drive with given id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Library.Drive>> Get(int id)
         {
-            return Ok(await data.GetDrives(company));
+            Library.Drive drive = await data.GetDrive(id);
+            if (drive == null)
+                return NotFound();
+            else
+                   return  Ok(drive);
+        }
+
+
+        /// <summary>
+        /// Get all drives from a driver from the given company
+        /// </summary>
+        /// <param name="company"></param>
+        /// <returns></returns>
+        [HttpGet("{company}/company")]
+        public async Task<ActionResult<List<Library.Drive>>> GetByCompany(string company)
+        {
+            return Ok(await data.GetDrivesByCompany(company));
+        }
+
+        /// <summary>
+        /// Get all drives for the user with given Id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        [HttpGet("{userId}/user")]
+        public async Task<ActionResult<List<Library.Drive>>> GetByUserId(int userId)
+        {
+            return Ok(await data.GetDrivesByUser(userId));
+        }
+        /// <summary>
+        /// Get all drives from the driver with given Id
+        /// </summary>
+        /// <param name="driverId"></param>
+        /// <returns></returns>
+        [HttpGet("{driverId}/driver")]
+        public async Task<ActionResult<List<Library.Drive>>> GetByDriverId(int driverId)
+        {
+            return Ok(await data.GetDrivesByDriver(driverId));
+        }
+
+        /// <summary>
+        /// Attempt to start a new drive
+        /// </summary>
+        /// <param name="drive"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult<Library.Drive>> New(Library.Drive drive)
+        {
+            try
+            {
+                var d = await data.NewDrive(drive);
+                return Ok(d);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest();
+            }
         }
 
     }
